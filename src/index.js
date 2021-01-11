5482d254b2f20c65b76b11832230e9b6c5b7017c
Daniel D. Beck <daniel@ddbeck.com>
2021-01-11 12:21:41 +0000

    Initial implementation

--- src/index.js
+++ src/index.js
@@ -1,44 +1,63 @@
 const core = require("@actions/core");
+const github = require("@actions/github");
+
+const isInvalid = require("./is-invalid");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const meta = {
-      token: core.getInput("token"),
-      repository: core.getInput("repository"),
-      issue_number: core.getInput("issue-number"),
+    const token = core.getInput("repo-token", { required: true });
+    const settings = {
+      labels: core.getInput("labels") || undefined,
+      comment: core.getInput("comment") || undefined,
     };
     const conditions = {
       title_contains: core.getInput("title-contains") || undefined,
       body_contains: core.getInput("body-contains") || undefined,
     };
 
-    core.info(`Getting details for ${meta.repository}#${meta.issue_number}…`);
+    core.debug("Getting GitHub issue context");
     const octokit = github.getOctokit(token);
-    const issue = getIssue(octokit, meta);
+    const context = github.context;
 
-    // TODO: check validity
+    core.debug(
+      `Getting details for ${context.repository}#${context.issue_number}`
+    );
+    const issue = await octokit.issues.get(context);
+
+    core.debug(
+      `Checking validity of ${context.repository}#${context.issue_number}`
+    );
+    if (isInvalid(issue, conditions)) {
+      await handleInvalidIssue(octokit, context, settings);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-async function getIssue(octokit, meta) {
-  octokit.issues.get(meta);
+async function handleInvalidIssue(octokit, context, settings) {
+  if (settings.labels) {
+    await labelIssue(octokit, context, settings.labels);
+  }
+  if (settings.comment) {
+    await commentOnIssue(octokit, context, settings.comment);
+  }
+  await closeIssue(octokit, context);
 }
 
-async function commentOnIssue(octokit, meta, comment) {
-  octokit.issues.createComment({ ...meta, body: comment });
+async function commentOnIssue(octokit, context, comment) {
+  octokit.issues.createComment({ ...context, body: comment });
 }
 
-async function labelIssue(octokit, meta, labels) {
+async function labelIssue(octokit, context, labels) {
   if (labels.length) {
-    octokit.issues.addLabels({ ...meta, labels: { labels } });
+    octokit.issues.addLabels({ ...context, labels: labels.split(",") });
   }
 }
 
-async function closeIssue(octokit, meta) {
-  octokit.issues.update({ ...meta, state: "closed" });
+async function closeIssue(octokit, context) {
+  octokit.issues.update({ ...context, state: "closed" });
 }
 
 run();
